a0eede59945408ab5cb2b5e16ca884876b3db86b
Fellipe Henrique <fellipehf@gmail.com>
2022-09-14 22:26:48 -0300

    fix: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,8 +3,9 @@ export default {
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-test'
+    '.+\\.ts$': 'ts-jest'
   }
 }
