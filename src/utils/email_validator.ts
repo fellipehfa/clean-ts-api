1966556c750b852890df95714bcaa578a75f037d
Fellipe Henrique <fellipehf@gmail.com>
2023-02-02 13:02:22 -0300

    fix: files names

--- src/utils/email_validator.ts
+++
@@ -1,7 +0,0 @@
-import { EmailValidator } from '../presentation/protocols/email-validator'
-
-export class EmailValidatorAdapter implements EmailValidator {
-  isValid (email: string): boolean {
-    return false
-  }
-}
