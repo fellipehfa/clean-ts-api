f5f04ad80ea9b2f1eeca5d52d5e8041521058d34
Fellipe Henrique <fellipehf@gmail.com>
2023-02-02 10:05:32 -0300

    refactor: add console error on try catch

--- src/presentation/controllers/signup/signUp.ts
+++ src/presentation/controllers/signup/signUp.ts
@@ -34,6 +34,7 @@ export class SignUpController implements Controller {
       })
       return success(account)
     } catch (error) {
+      console.error(error)
       return serverError()
     }
   }
