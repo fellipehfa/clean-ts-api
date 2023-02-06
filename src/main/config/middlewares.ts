e2b125e81facecdfac403dd929e7adde8ed3627c
Fellipe Henrique <fellipehf@gmail.com>
2023-02-06 20:48:33 -0300

    feat: ensure Content Type Middleware returns json as default

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bosyParser } from '../middlewares/body-parser'
+import { contType } from '../middlewares/content-type'
 import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(bosyParser)
   app.use(cors)
+  app.use(contType)
 }
