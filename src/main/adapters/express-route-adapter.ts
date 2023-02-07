17706cb57fae10da570811a0b80e188bd580a5a7
Fellipe Henrique <fellipehf@gmail.com>
2023-02-07 11:34:38 -0300

    refactor: type a const

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,10 +1,10 @@
-import { Controller, HttpRequest } from '../../presentation/protocols'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { Request, Response } from 'express'
 
 export const adapterRoute = async (controller: Controller): Promise<any> => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = { body: req.body }
-    const httpResponse = await controller.handle(httpRequest)
+    const httpResponse: HttpResponse = await controller.handle(httpRequest)
     res.status(httpResponse.statusCode).json(httpResponse.body)
   }
 }
