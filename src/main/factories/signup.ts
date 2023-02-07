0d19fe0ff398dcfa307a33dd25ea7f5436fc945a
Fellipe Henrique <fellipehf@gmail.com>
2023-02-06 23:04:21 -0300

    fix: files names

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,7 +1,7 @@
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
-import { SignUpController } from '../../presentation/controllers/signup/signUp'
+import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 
 export const makeSignUpController = (): SignUpController => {
