2c62d32ef865406acfa1db68d43d0cd753402587
Fellipe Henrique <fellipehf@gmail.com>
2023-02-07 22:50:25 -0300

    feat: add LoginController and ensure LoginController returns 400 if a required params is not provided

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,15 @@
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helper/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class LoginController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['email', 'password']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+    return httpRequest.body
+  }
+}
