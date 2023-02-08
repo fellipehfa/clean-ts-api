391964a7730c254c9b2b2ca04601389abc2edabf
Fellipe Henrique <fellipehf@gmail.com>
2023-02-07 21:20:11 -0300

    test: ensure LogMongoRepository creates a error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,32 @@
+import { Collection } from 'mongodb'
+import { LogErrorRepository } from '../../../../data/protocols/log-error-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+describe('Log Error Mongo Repository', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany()
+  })
+
+  const makeSut = (): LogErrorRepository => {
+    return new LogMongoRepository()
+  }
+
+  test('Should create a log error on success', async () => {
+    const sut = makeSut()
+    await sut.logError('Error message')
+    const error = await errorCollection.findOne()
+    expect(error.stack).toEqual('Error message')
+  })
+})
