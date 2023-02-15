60f7f07e0ce976ef27bce4dab4e98927945f8abb
Fellipe Henrique <fellipehf@gmail.com>
2023-02-14 23:53:57 -0300

    feat: ensure UpdateAccessTokenRepository works as DbAuthentication expected

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,18 +1,21 @@
-import { Authentication, AuthenticationModel, HashCompare, LoadAccountByEmailRepository, TokenGenerate } from './db-authentication-protocols'
+import { Authentication, AuthenticationModel, HashCompare, LoadAccountByEmailRepository, TokenGenerate, UpdateAccessTokenRepository } from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashCompare: HashCompare
   private readonly tokenGenerate: TokenGenerate
+  private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
 
   constructor (
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashCompare: HashCompare,
-    tokenGenerate: TokenGenerate
+    tokenGenerate: TokenGenerate,
+    updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashCompare = hashCompare
     this.tokenGenerate = tokenGenerate
+    this.updateAccessTokenRepository = updateAccessTokenRepository
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -20,8 +23,9 @@ export class DbAuthentication implements Authentication {
     if (account) {
       const isCompatible = await this.hashCompare.compare(authentication.password, account.password)
       if (isCompatible) {
-        const token = await this.tokenGenerate.generate(account.id)
-        return token
+        const accessToken = await this.tokenGenerate.generate(account.id)
+        if (accessToken) await this.updateAccessTokenRepository.update(account.id, accessToken)
+        return accessToken
       }
     }
     return null
