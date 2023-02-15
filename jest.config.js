426a4c7e845afb937bd1d8e9d13baed65ccf3fe4
Fellipe Henrique <fellipehf@gmail.com>
2023-02-15 01:08:46 -0300

    chore: add jest configs

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,15 @@ module.exports = {
     '!<rootDir>/src/main/**',
     '!<rootDir>/src/presentation/protocols/index.ts',
     '!<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts',
+    '!<rootDir>/src/data/usecases/authentication/db-authentication-protocols.ts',
     '!<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
     '!<rootDir>/src/presentation/controllers/login/login-protocols.ts',
     '!<rootDir>/src/presentation/helper/validator/required-field-validation.ts',
     '!<rootDir>/src/presentation/helper/validator/validation-composite.ts',
     '!<rootDir>/src/presentation/helper/validator/email-validation.ts',
-    '!<rootDir>/src/presentation/helper/validator/compare-field-validation.ts'
+    '!<rootDir>/src/presentation/helper/validator/compare-field-validation.ts',
+    '!<rootDir>/src/data/protocols/cryptography/index.ts',
+    '!<rootDir>/src/data/protocols/db/index.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
