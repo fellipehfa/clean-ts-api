04d9993897e64b83dcf63492151891645f413fae
Fellipe Henrique <fellipehf@gmail.com>
2023-04-04 22:56:26 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
   MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   SERVER_HOST: process.env.SERVER_HOST || 'http://localhost',
-  SERVER_PORT: process.env.MONGO_PORT || 5050
+  SERVER_PORT: process.env.MONGO_PORT || 5050,
+  JWT_SECRET: process.env.JWT_SECRET || 'as@42rs0E'
 }
---
+++ src/main/factories/login/login-factory.ts
@@ -0,0 +1,21 @@
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
+import { JWTAdapter } from '../../../infra/cryptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
+import { LoginController } from '../../../presentation/controllers/login/login-controller'
+import { Controller } from '../../../presentation/protocols'
+import env from '../../config/env'
+import { LogControllerDecorator } from '../../decorators/log/log-controller-decorator'
+import { makeLoginValidation } from './login-validation-factory'
+
+export const makeLoginController = (): Controller => {
+  const SALT = 12
+  const accountMongoRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(SALT)
+  const jwtAdapter = new JWTAdapter(env.JWT_SECRET)
+  const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+  const loginController = new LoginController(dbAuthentication, makeLoginValidation())
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(loginController, logMongoRepository)
+}
